7d44dab70cb266952b38f5e9e89662fecdddd670
goldenvertex42 <fabled93@gmail.com>
2026-03-18 11:13:25 -0500

    feat(validation): implement validation of form before controller runs middleware on user input

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,8 +4,9 @@ const productRouter = Router();
 
 // Important: Put '/new' BEFORE '/:id' so Express doesn't think 'new' is an ID
 productRouter.get("/new", productController.productCreateGet);
-productRouter.post("/new", productController.productCreatePost);
+productRouter.post("/new", productController.validateProduct, productController.productCreatePost);
 productRouter.get("/:id/edit", productController.productUpdateGet);
-productRouter.post("/:id/edit", productController.productUpdatePost);
+productRouter.post("/:id/edit", productController.validateProduct, productController.productUpdatePost);
+productRouter.post("/:id/delete", productController.productDeletePost);
 
 module.exports = productRouter;
