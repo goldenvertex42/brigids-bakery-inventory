eb9dff027b82f27e5be44821b1cc089abc43d1c5
goldenvertex42 <fabled93@gmail.com>
2026-03-23 13:48:02 -0500

    chore: replace connection variables with connection string

--- db/pool.js
+++ db/pool.js
@@ -3,8 +3,5 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: process.env.DB_HOST, // or wherever the db is hosted
-  user: process.env.DB_USER,
-  database: process.env.DB,
-  port: Number(process.env.DB_PORT) // The default port
+  connectionString: process.env.DATABASE_URL // The default port
 });
